0be91e3ef84c0c0287dc96964d2a86b24c006b15
Alejandro Mechine Gamarra <alejandromechinegamarra@gmail.com>
2017-10-03 12:43:05 +0200

    commit Inicial

---
+++ app/src/main/java/com/example/contadordecafes/ContadorDeCafes.java
@@ -0,0 +1,13 @@
+package com.example.contadordecafes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContadorDeCafes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contador_de_cafes);
+    }
+}
